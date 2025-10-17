ed053343d048658df9259c0e4161d94a9c9feaa9
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-17 18:51:00 -0400

    Se trabajo el tipoHuacales con los atributos necesarios

---
+++ DTO/TiposHuacalesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GestionHuacales.Api.DTO;
+
+public class TiposHuacalesDto
+{
+    public int TipoId { get; set; }
+    public string Descripcion { get; set; }
+    public int Existencia { get; set; }
+}
